5dd7b13004118c42c920b3f66267e13b0fb93093
gwanwoo.kim <gwanwoo@aaa.com>
2021-03-02 19:46:55 +0900

    기본 프로젝트 생성

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,5 +1,23 @@
 package main;
 
-public class MainClass {
+import java.util.Scanner;
 
+public class MainClass {
+	public static void main(String[] args) {
+		System.out.println("main 시작");
+		// kim - 로그인 관련 기능을 만드세요
+		// cho - 회원 관리 기능을 만드세요
+		Scanner input = new Scanner(System.in);
+		int num;
+		while(true) {
+			System.out.println("1.로그인 2.회원관리");
+			num = input.nextInt();
+			switch(num) {
+			case 1:
+				break;
+			case 2:
+				break;
+			}
+		}
+	}
 }
